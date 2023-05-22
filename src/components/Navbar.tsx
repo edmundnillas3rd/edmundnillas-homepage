2bc5254cfb4d3d1c937702c6eeb5858a876f4875
Edmund Nillas III <edmundnillas3rd@gmail.com>
2023-05-23 07:55:39 +0800

    Add responsive style for navbar (branch: dev-overhaul

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,22 +1,27 @@
 import NextLink from "next/link";
-import { Flex, Link, Spacer, Text } from "@chakra-ui/react";
+import { Flex, Link, Spacer, Text, Container } from "@chakra-ui/react";
 
 const Navbar = () => (
-  <Flex p={20}>
-    <Text>Edmund Nillas III</Text>
-    <Spacer />
-    <Flex gap={10}>
-      <Link as={NextLink} href="/">
-        Home
-      </Link>
-      <Link as={NextLink} href="/blogs">
-        Blogs
-      </Link>
-      <Link as={NextLink} href="/about">
-        About
-      </Link>
+  <Container maxW="container.lg" py={5}>
+    <Flex>
+      <Text>Edmund Nillas III</Text>
+      <Spacer />
+      <Flex>
+        <Flex gap={10} display={{ base: "none", md: "flex", lg: "flex" }}>
+          <Link as={NextLink} href="/">
+            Home
+          </Link>
+          <Link as={NextLink} href="/blogs">
+            Blogs
+          </Link>
+          <Link as={NextLink} href="/about">
+            About
+          </Link>
+        </Flex>
+        <Text display={{ base: "block", sm: "block", md: "none" }}>Menu</Text>
+      </Flex>
     </Flex>
-  </Flex>
+  </Container>
 );
 
 export default Navbar;
