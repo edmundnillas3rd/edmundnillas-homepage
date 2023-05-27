0cb8927ca94251e8ae0a469233fc56f87102b6ee
Edmund Nillas III <edmundnillas3rd@gmail.com>
2023-05-27 21:01:02 +0800

    Add configuration for static params

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -3,13 +3,20 @@
 import { Container, Text } from "@chakra-ui/react";
 
 const Blogs = ({ params }) => {
-  //   const response = fetch(`${process.env.REACT_APP_BASE_URL}`);
-
   return (
     <Container size="container.lg">
       <Text>Coming Soon...</Text>
     </Container>
   );
 };
+
+export async function generateStaticParams() {
+  // TODO (Edmund): URL to be added for the fetch API function
+  const posts = await fetch("").then((res) => res.json());
+
+  return posts.map((post) => ({
+    slug: post.slug,
+  }));
+}
 
 export default Blogs;
