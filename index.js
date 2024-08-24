1b36b5827ac5522d5b0c698fa08fc6345f05ebe9
Edmund Nillas III <edmundnillas3rd@gmail.com>
2024-08-24 17:02:29 +0800

    Add scene environment for lighting solutions

--- index.js
+++ index.js
@@ -1,35 +1,53 @@
 import * as THREE from 'three';
-import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 0.1, 1000 );
+import { RoomEnvironment } from 'three/examples/jsm/Addons.js';
+
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
 const canvasRef = document.getElementById("landpage-canvas");
 const renderer = new THREE.WebGLRenderer({ canvas: canvasRef });
 renderer.setSize( window.innerWidth, window.innerHeight );
-renderer.setClearColor(0xff0000);
 
-const directionalLight = new THREE.DirectionalLight( 0xffffff, 0.5 );
-scene.add( directionalLight );
+const pmremGenerator = new THREE.PMREMGenerator(renderer);
+
+const scene = new THREE.Scene();
+scene.background = new THREE.Color(0x1A1B1C);
+scene.environment = pmremGenerator.fromScene(new RoomEnvironment(renderer), 0.04).texture;
+
+const camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
 let model = null;
 
 const loader = new GLTFLoader();
 loader.load("models/crt_tv/scene.gltf", function (gltf) {
     model = gltf.scene;
+
+    model.position.x = -1.5;
+    model.position.y = 0.1;
+    model.position.z = 498;
+
+    const quaternion = new THREE.Quaternion();
+    quaternion.setFromAxisAngle(new THREE.Vector3(0, 1, 0).normalize(),  4.0);
+    model.applyQuaternion(quaternion);
+    quaternion.setFromAxisAngle(new THREE.Vector3(0, 0, 1).normalize(), -0.3);
+    model.applyQuaternion(quaternion);
+
     scene.add(model);
+
 }, undefined, function (err) {
     console.error(err);
 });
 
-camera.position.z = 5;
-
+// Starting coordinates of the first scene (landing page) will be:
+// [X]: center of the screen
+// [Y]: center of the screen
+// [z]: 500px from the origin of the scene
+camera.position.z = 500;
 
 function animate() {
-    if (model) {
-        model.rotation.y += 0.01;
-    }
 	renderer.render( scene, camera );
 }
 
 renderer.setAnimationLoop(animate);
+
+scene.remove(model);
