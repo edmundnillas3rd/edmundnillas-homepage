8d6c1a33d8916d049bbb3f223d74f3ac718e0d3c
Edmund Nillas III <edmundnillas3rd@gmail.com>
2024-08-25 16:12:28 +0800

    Add lerping animation for first instance of loaded models

--- index.js
+++ index.js
@@ -4,50 +4,55 @@ import { RoomEnvironment } from 'three/examples/jsm/Addons.js';
 
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
-const canvasRef = document.getElementById("landpage-canvas");
-const renderer = new THREE.WebGLRenderer({ canvas: canvasRef });
-renderer.setSize( window.innerWidth, window.innerHeight );
-
-const pmremGenerator = new THREE.PMREMGenerator(renderer);
-
-const scene = new THREE.Scene();
-scene.background = new THREE.Color(0x1A1B1C);
-scene.environment = pmremGenerator.fromScene(new RoomEnvironment(renderer), 0.04).texture;
-
-const camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 0.1, 1000 );
-
-let model = null;
-
-const loader = new GLTFLoader();
-loader.load("models/crt_tv/scene.gltf", function (gltf) {
-    model = gltf.scene;
-
-    model.position.x = -1.5;
-    model.position.y = 0.1;
-    model.position.z = 498;
-
-    const quaternion = new THREE.Quaternion();
-    quaternion.setFromAxisAngle(new THREE.Vector3(0, 1, 0).normalize(),  4.0);
-    model.applyQuaternion(quaternion);
-    quaternion.setFromAxisAngle(new THREE.Vector3(0, 0, 1).normalize(), -0.3);
-    model.applyQuaternion(quaternion);
-
-    scene.add(model);
-
-}, undefined, function (err) {
-    console.error(err);
-});
-
-// Starting coordinates of the first scene (landing page) will be:
-// [X]: center of the screen
-// [Y]: center of the screen
-// [z]: 500px from the origin of the scene
-camera.position.z = 500;
-
-function animate() {
-	renderer.render( scene, camera );
+function initComponents(event) {
 }
 
-renderer.setAnimationLoop(animate);
+function init(event) {
+    initComponents();
 
-scene.remove(model);
+    const canvasRef = document.getElementById("landpage-canvas");
+    const renderer = new THREE.WebGLRenderer({ canvas: canvasRef });
+    renderer.setSize( window.innerWidth, window.innerHeight );
+
+    const pmremGenerator = new THREE.PMREMGenerator(renderer);
+
+    const scene = new THREE.Scene();
+    scene.background = new THREE.Color(0x1A1B1C);
+    scene.environment = pmremGenerator.fromScene(new RoomEnvironment(renderer), 0.04).texture;
+
+    const camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+    let model = null;
+
+    const loader = new GLTFLoader();
+    loader.load("models/landing_page_scene/scene.gltf", function (gltf) {
+        model = gltf.scene;
+        model.position.set(0, 0, 498);
+        model.scale.setScalar(0);
+
+        scene.add(model);
+
+    }, undefined, function (err) {
+        console.error(err);
+    });
+
+    scene.remove(model);
+
+    // Starting coordinates of the first scene (landing page) will be:
+    // [X]: center of the screen
+    // [Y]: center of the screen
+    // [z]: 500px from the origin of the scene
+
+    camera.position.z = 500;
+
+    function animate() {
+        if (model) {
+            model.scale.lerp(new THREE.Vector3(1, 1, 1), 0.25);
+        }
+        renderer.render( scene, camera );
+    }
+
+    renderer.setAnimationLoop(animate);
+}
+
+window.addEventListener("DOMContentLoaded", init);
